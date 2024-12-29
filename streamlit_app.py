957999ab78d0f53a8addb610eda7dad6e6c05e9e
SisNatt <nattarin.db@gmail.com>
2024-12-29 14:32:50 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-st.title('🎈 App Name')
+st.title(' 👮‍♀️🤖 Data Loss Prevention ML report 👾')
 
-st.write('Hello world!')
+st.write('This is report  builds a  machine learning model!')
