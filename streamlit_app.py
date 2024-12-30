ef793186ab278177d2c2302b18503b7088511be9
SisNatt <nattarin.db@gmail.com>
2024-12-30 12:25:55 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ os.makedirs(OUTPUT_DIR, exist_ok=True)
 selected = option_menu(
     "Main Menu", 
     ["Home - Raw Data", "View Processed Data", "Pattern Mining"], 
-    icons=['house', 'search', 'bar-chart', 'diagram-3'], 
+    icons=['house', 'bar-chart', 'diagram-3'], 
     menu_icon="cast", 
     default_index=0
 )
